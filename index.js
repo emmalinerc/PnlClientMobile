6847813870112a16d86b00c66645d8d6ab7eca98
Emmaline Cushey <emmalinecushey@Emmalines-MacBook-Pro.local>
2019-02-25 13:30:17 -0500

    fixed android auth0

--- index.js
+++ index.js
@@ -26,8 +26,89 @@ const auth0 = new Auth0(credentials);
    
   constructor(props) {
     super(props);
-    this.state = { accessToken: null };
+    this.state = { 
+      accessToken: null, 
+      currentUser: "",
+      locations: [],
+      doors: [],
+      favorites: [],
+      currentPage: "locations",
+      showLocations: true,
+      loggedIn: false
+
+    };
+    // this.onHandleBottomNavLocationsClick = this.onHandleBottomNavLocationsClick.bind(
+    //   this
+    // );
+  
+    // this.onHandleBottomNavFavoritesClick = this.onHandleBottomNavFavoritesClick.bind(
+    //   this
+    // );
+    // this.handleBottonNavSignOutClick = this.handleBottonNavSignOutClick.bind(
+    //   this
+    // );
+    // this.getLocationsList = this.getLocationsList.bind(this);
+    // this.showDoors = this.showDoors.bind(this);
+    // this.onDoorClick = this.onDoorClick.bind(this);
+    // this.logout = this.logout.bind(this);
   }
+  
+
+
+
+// getLocationsList() {
+//   const { getIdToken } = this.props.auth;
+//   const headers = { authorization: `Bearer ${getIdToken()}` };
+
+//   axios
+//     .get(APP_CONFIG.server_base_url + "client/doors", { headers })
+//     .then(res => {
+//       console.log(res);
+//       this.setState({ locations: res.data.locations });
+//     })
+//     .catch(err => console.log(err));
+// }
+
+// componentDidMount() {
+//   this.getLocationsList();
+// }
+
+// onDoorClick(id) {
+//   const { getIdToken } = this.props.auth;
+//   const headers = { authorization: `Bearer ${getIdToken()}` };
+
+//   axios
+//     .get(APP_CONFIG.server_base_url + "client/open?Id=" + id, {
+//       headers
+//     })
+//     .then(res => console.log(res))
+//     .catch(err => console.log(err));
+// }
+
+// onHandleBottomNavLocationsClick() {
+//   this.setState({ currentPage: "locations" });
+//   this.setState({ showLocations: true });
+//   console.log(this.state.currentPage);
+// }
+
+// onHandleBottomNavFavoritesClick() {
+//   this.setState({ currentPage: "favorites" });
+//   console.log(this.state.currentPage);
+// }
+
+// handleBottonNavSignOutClick() {
+//   this.setState({ currentPage: "signout" });
+//   console.log(this.state.currentPage);
+// }
+
+// logout() {
+//   this.props.auth.logout();
+// }
+
+// showDoors() {
+//   this.setState({ showLocations: false });
+// }
+
 
   _onLogin = () => {
 
